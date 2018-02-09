4c9393edd9e8e37f26e9c8fa029f3974fa396344
Peter Otto Kuhberg <peter@kuhberg.dk>
2018-02-09 15:19:17 +0100

    sorting flights now works, and it generates and list of results TODO : find a way to load the rest of the html in a smarter way

--- friendly-flights/index.js
+++ friendly-flights/index.js
@@ -18,14 +18,23 @@ app.use(bodyParser.urlencoded({     // to support URL-encoded bodies
  
 app.listen(port);
 
+var fligtRes;
 app.post('/', function(req, res) {
-	res.sendStatus(200);
+	fligtRes = res;
  	// sending a response does not pause the function
  	getFlights(req.body);
 });
 
+var url = "https://api.sandbox.amadeus.com/v1.2/flights/inspiration-search?apikey=GrBn3mBuHGEGD1Xr1xf9vlzBpo7AHSLD"
+
+var urlOne;
 var originOne;
+var destinationsOne;
+
+var urlTwo;
 var originTwo;
+var destinationsTwo;
+
 var destination;
 var dates;
 var duration;
@@ -38,16 +47,74 @@ function getFlights(postData){
 	dates = postData.daterange;
 	duration = postData.duration;
 	maxPrice = postData.maxPrice;
+	//rearanging the dates so they match what the api is expecting
+	dates = dates.substr(6,4)+"-"+dates.substr(0,2)+"-"+dates.substr(3,2)+"--"+dates.substr(19,4)+"-"+dates.substr(13,2)+"-"+dates.substr(16,2);
 
-	var dates = dates.substr(6,4)+"-"+dates.substr(0,2)+"-"+dates.substr(3,2)+"--"+dates.substr(19,4)+"-"+dates.substr(13,2)+"-"+dates.substr(16,2);
+	if (destination !== "undefined") {
+		url += "&destination="+destination;
+	}
 
-	console.log(originOne + " : " + originTwo + " : " + destination + " : " + dates + " : " + duration + " : " + maxPrice);
+	if (dates !== "undefined") {
+		url += "&departure_date="+dates;
+	}
 
-	var url = "https://api.sandbox.amadeus.com/v1.2/flights/inspiration-search?apikey=GrBn3mBuHGEGD1Xr1xf9vlzBpo7AHSLD&origin="+originOne+"&destination="+destination+"&departure_date="+dates
+	if(maxPrice) {
+		url += "&max_price="+maxPrice;
+	}
+
+	if(duration) {
+		url += "&duration="+duration;
+	}
+
+	if (originOne !== "undefined") {
+		urlOne = url;
+		urlOne  += "&origin="+originOne;
+	}
+
+	if (originTwo !== "undefined") {
+		urlTwo = url;
+		urlTwo  += "&origin="+originTwo;
+	}
 	
-	request.get(url, function(err, res, body){
+	request.get(urlOne, function(err, res, body){
 		if (err){} //TODO: handle error
 		if (res.statusCode !== 200) {}
-		else console.log(body);
+		else {
+			destinationsOne = JSON.parse(body);
+			if (destinationsOne && destinationsTwo) {
+				sortFlights(destinationsOne, destinationsTwo);
+			}
+		}
+	});
+
+	request.get(urlTwo, function(err, res, body){
+		if (err){} //TODO: handle error
+		if (res.statusCode !== 200) {}
+		else {
+			destinationsTwo = JSON.parse(body);
+			if (destinationsOne && destinationsTwo) {
+				sortFlights(destinationsOne, destinationsTwo);
+			}
+		}
 	});
 };
+var buf = '<div> <p> These are the result that we found </p> </div>';
+function sortFlights(destinationsOne, destinationsTwo){
+	
+	for (var i = destinationsOne.results.length - 1; i >= 0; i--) {
+		for (var e = destinationsTwo.results.length - 1; e >= 0; e--) {
+			if (destinationsOne.results[i].destination == destinationsTwo.results[e].destination) {
+				if (destinationsOne.results[i].departure_date == destinationsTwo.results[e].departure_date) {
+					var tmp = '<ul><li> Destination : ' + destinationsOne.results[i].destination + '</li><li>Departure date : ' + destinationsOne.results[i].departure_date + '</li><li>Return date : ' + destinationsOne.results[i].return_date + '</li><li>From ' + originOne + ' : ' + destinationsOne.results[i].price + ' ' + destinationsOne.currency + '</li><li>From ' + originTwo + ' : ' + destinationsTwo.results[e].price + ' ' + destinationsTwo.currency + '</li></ul>';
+					buf += tmp;
+				}
+			}
+		}
+	}
+	console.log("I RAN");
+	if (fligtRes.statusCode === 200) {
+        fligtRes.write('<html><body><p>' + buf + '</p></body></html>');
+        fligtRes.end();
+		//fligtRes.sendFile( path.join( __dirname, 'public', 'flights.html' ));
+	}
+}
--- friendly-flights/public/assets/js/functions.js
+++ friendly-flights/public/assets/js/functions.js
@@ -17,3 +17,7 @@ $(function() {
 	  console.log("New date range selected: " + start.format('YYYY-MM-DD') + " to " + end.format('YYYY-MM-DD') + " (predefined range: " + label + ")");
 	});
 });
+
+function search(){
+	document.getElementById("searchModal").style.display = "block";
+}
